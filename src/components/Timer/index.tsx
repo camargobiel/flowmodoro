5a06723419f8deb215850cab157ca81d8ab357f4
camargobiel <camargoobiel@gmail.com>
2024-01-17 16:44:40 -0300

    feat: 🎸 more zero in frontof

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -8,7 +8,7 @@ export const Timer = ({ seconds }: TimerProps) => {
   const [minutes, secondsLeft] = formatSeconds(seconds);
 
   return (
-    <div className="flex gap-1 text-7xl font-semibold items-center w-40">
+    <div className="flex gap-1 text-7xl font-semibold items-center w-48">
       <div>{minutes}</div>
       <div className="mb-3">:</div>
       <div>{secondsLeft}</div>
--- src/utils/formatSeconds/index.ts
+++ src/utils/formatSeconds/index.ts
@@ -1,5 +1,7 @@
 export const formatSeconds = (seconds: number) => {
   const minutes = Math.floor(seconds / 60);
   const secondsLeft = seconds % 60;
-  return [minutes, secondsLeft < 10 ? `0${secondsLeft}` : secondsLeft];
+  const formattedSeconds = secondsLeft < 10 ? `0${secondsLeft}` : secondsLeft
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes
+  return [formattedMinutes, formattedSeconds];
 }
